ce5279621f78ae11462175a1209c57c27ba4394e
saitam91 <msilva2091@gmail.com>
2022-08-10 13:00:20 -0300

    Upload

--- src/test/groovy/TestMELI.groovy
+++ src/test/groovy/TestMELI.groovy
@@ -13,15 +13,19 @@ class TestMELI extends BaseMELI{
 
     @Test(groups = "TestMeli",dataProvider="DataContainer", dataProviderClass = BaseMELI.class)
     public void criteriaMatchesWithResult(String searchCriteria) {
-        Response response=get("/sites/MLA/search?q="+searchCriteria)
-        String [] resultSearch = response.path("results.title")
 
-       String[] criteria = searchCriteria.split(" ");
+        Response response=get("/sites/MLA/search?q="+searchCriteria)
+
+        String [] resultSearch = response.path("results.title")
+        String[] criteria = searchCriteria.split(" ");
+
         for (int i = 0; i < resultSearch.length; i++) {
             for (int  j= 0; j < criteria.length; j++){
+
            Boolean upperCaseResultSearch = resultSearch[i].contains(criteria[j].substring(0, 1).toUpperCase() + criteria[j].substring(1));
            Boolean lowerCaseResultSearch = resultSearch[i].contains(criteria[j].substring(0, 1).toLowerCase() + criteria[j].substring(1));
-                       assertTrue("The result search "+resultSearch[i]+" not match with the search criteria: "+criteria[j],(upperCaseResultSearch || lowerCaseResultSearch));
+
+                assertTrue("The result search "+resultSearch[i]+" not match with the search criteria: "+criteria[j],(upperCaseResultSearch || lowerCaseResultSearch));
                        log.info("The result search "+resultSearch[i]+" matches with the search criteria: "+criteria[j]);
             }
             }
